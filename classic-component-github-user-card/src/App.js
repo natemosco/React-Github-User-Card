2e1b167f6892d6d65179d422fffc9221ec9c9d88
Nathaniel Mosco <natemosco@gmail.com>
2019-10-29 17:31:30 -0400

    Initial Setup for app

---
+++ classic-component-github-user-card/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import axios from "axios";
+
+class App extends React.Component() {
+  state = {
+    github: []
+  }
+
+  componentDidMount() {
+    axios
+      .get(' https://api.github.com/users/natemosco')
+      .then(res => {
+        console.log(res, "res from initial axios request")
+        // this.setState({
+        // github:[res.data]
+        // })
+      })
+  }
+  // componentDidUpdate(){
+  //   axios
+  // }
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+
+  };
+};
+
+export default App;
